d4c55915dd82605f75375ca8689a4f5780669b3c
Jonathan Liu <liu.z.jonathan@gmail.com>
2023-11-21 00:41:59 -0600

    implemented posthog for blog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material"
 import "@/styles/globals.css"
 import { DM_Sans } from "next/font/google"
+import { PHProvider, PostHogPageview } from "./providers"
+import { Suspense } from "react"
 
 export const dmSans = DM_Sans({
   weight: ["400", "500", "600", "700"],
@@ -14,10 +16,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={dmSans.className}>
-        <CssBaseline />
-        {children}
-      </body>
+      <Suspense>
+        <PostHogPageview />
+      </Suspense>
+      <PHProvider>
+        <body className={dmSans.className}>
+          <CssBaseline />
+          {children}
+        </body>
+      </PHProvider>
     </html>
   )
 }
