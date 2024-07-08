9b1007cdc520ddf4de803c2c38fde4efb0100c7b
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-07-08 02:05:24 -0500

    updated robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/e/",
+      disallow: ["/e/", "/g/"],
     },
     sitemap: "https://schej.it/sitemap.xml",
   }
