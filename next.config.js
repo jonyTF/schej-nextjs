aac912d49d0967805d50cd97e7a22b40f22d2c32
Jonathan Liu <liu.z.jonathan@gmail.com>
2025-05-05 17:20:22 -0700

    got rid of rewrites

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return {
-      fallback: [
-        {
-          source: "/:path*",
-          destination: "http://3.18.115.80/:path*",
-        },
-      ],
-    }
-  },
+  // output: "export",
+  // async rewrites() {
+  //   return {
+  //     fallback: [
+  //       {
+  //         source: "/:path*",
+  //         destination: "http://3.18.115.80/:path*",
+  //       },
+  //     ],
+  //   }
+  // },
 }
 
 module.exports = nextConfig
